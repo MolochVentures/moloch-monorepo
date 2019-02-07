8b5d7c237dc733c8413819931617b711f73e74e0
James Young <james.s.young@gmail.com>
2019-02-07 15:43:30 -0700

    updated migrations, config, add SimpleToken and simple test to submit membership

--- packages/moloch/migrations/2_moloch_migration.js
+++ packages/moloch/migrations/2_moloch_migration.js
@@ -1,35 +1,25 @@
 /* global artifacts */
 const BigNumber = require('bignumber.js')
 const Moloch = artifacts.require('./Moloch.sol')
-// const LootToken = artifacts.require('./LootToken.sol')
-const GuildBank = artifacts.require('./GuildBank.sol')
-const FooToken = artifacts.require('./oz/ERC20.sol')
-const BarToken = artifacts.require('./oz/ERC20.sol')
+const SimpleToken = artifacts.require('./oz/SimpleToken.sol')
 
-const foundersJSON = require('./founders.json')
 const configJSON = require('./config.json')
-
 
 module.exports = (deployer, network, accounts) => {
   deployer.then(async () => {
-    fooToken  = await deployer.deploy(FooToken)
-    guildBank = await deployer.deploy(
-      GuildBank,
-      fooToken.address
-    )
+    const simpleToken  = await deployer.deploy(SimpleToken)
+
     moloch = await deployer.deploy(
       Moloch,
       accounts[0],
-      fooToken.address,
-      17280,
-      7,
-      7,
-      new BigNumber(10000000000000000000),
-      3,
-      new BigNumber(100000000000000000),
+      simpleToken.address,
+      configJSON.PERIOD_DURATION,
+      configJSON.VOTING_PERIOD_LENGTH,
+      configJSON.GRACE_PERIOD_LENGTH,
+      new BigNumber(configJSON.PROPOSAL_DEPOSIT),
+      configJSON.DILUTION_BOUND,
+      new BigNumber(configJSON.PROCESSING_REWARD),
       { gas: 6000000 }
     )
-    let proposalDeposit  = await moloch.proposalDeposit()
-    console.log('proposalDeposit', proposalDeposit.toString())
   })
 }
---
+++ packages/moloch/test/simpleMoloch.js
@@ -0,0 +1,135 @@
+/* global artifacts, contract, assert, web3 */
+/* eslint-env mocha */
+
+const Moloch = artifacts.require('./Moloch')
+const SimpleToken = artifacts.require('./SimpleToken')
+const configJSON = require('../migrations/config.json')
+
+const abi = require('web3-eth-abi')
+const HttpProvider = require(`ethjs-provider-http`)
+const EthRPC = require(`ethjs-rpc`)
+const ethRPC = new EthRPC(new HttpProvider('http://localhost:8545'))
+
+const BigNumber = require('bignumber.js')
+
+const should = require('chai').use(require('chai-as-promised')).use(require('chai-bignumber')(BigNumber)).should()
+
+async function blockTime() {
+  return (await web3.eth.getBlock('latest')).timestamp
+}
+
+function getEventParams(tx, event) {
+  if (tx.logs.length > 0) {
+    for (let idx=0; idx < tx.logs.length; idx++) {
+      if (tx.logs[idx].event == event) {
+        return tx.logs[idx].args
+      }
+    }
+  }
+  return false
+}
+
+async function snapshot() {
+  return new Promise((accept, reject) => {
+    ethRPC.sendAsync({method: `evm_snapshot`}, (err, result)=> {
+      if (err) {
+        reject(err)
+      } else {
+        accept(result)
+      }
+    })
+  })
+}
+
+async function restore(snapshotId) {
+  return new Promise((accept, reject) => {
+    ethRPC.sendAsync({method: `evm_revert`, params: [snapshotId]}, (err, result) => {
+      if (err) {
+        reject(err)
+      } else {
+        accept(result)
+      }
+    })
+  })
+}
+
+async function moveForwardSecs(secs) {
+  await ethRPC.sendAsync({
+    jsonrpc:'2.0', method: `evm_increaseTime`,
+    params: [secs],
+    id: 0
+  }, (err)=> {`error increasing time`});
+  const start = Date.now();
+  while (Date.now() < start + 300) {}
+  await ethRPC.sendAsync({method: `evm_mine`}, (err)=> {});
+  while (Date.now() < start + 300) {}
+  return true
+}
+
+web3.currentProvider.send({
+  jsonrpc: '2.0', 
+  method: 'evm_increaseTime', 
+  params: [10000], 
+  id: new Date().getSeconds()
+}, (err, resp) => {
+  if (!err) {
+    web3.currentProvider.send({
+    jsonrpc: '2.0', 
+    method: 'evm_mine', 
+    params: [], 
+    id: new Date().getSeconds()
+  }
+  }
+})
+
+contract('Moloch', accounts => {
+  let snapshotId
+
+  before('deploy contracts', async () => {
+    moloch = await Moloch.deployed()
+    simpleToken = await SimpleToken.deployed()
+
+    summoner = accounts[0]
+    applicant = accounts[1]
+    // transfer 10 SIM to applicant
+    simpleToken.transfer(applicant, new BigNumber("10000000000000000000"))
+    
+    // approve 10 SIM owner summoner and applicant to Moloch contract (spender)
+    simpleToken.approve(moloch.address, new BigNumber("10000000000000000000"))
+    simpleToken.approve(moloch.address, new BigNumber("10000000000000000000"), {from:applicant})
+  })
+
+  beforeEach(async () => {
+    snapshotId = await snapshot()
+  })
+
+  afterEach(async () => {
+    await restore(snapshotId)
+  })
+
+  it('verify deployment parameters', async () => {
+    const now = await blockTime() 
+    const member = await moloch.members(summoner)
+    const summoningTime =  await moloch.summoningTime()
+
+    assert.equal(member.isActive, true)
+    assert.equal(await moloch.approvedToken(), simpleToken.address)
+    assert.equal(await moloch.periodDuration(), configJSON.PERIOD_DURATION)
+    assert.equal(await moloch.votingPeriodLength(), configJSON.VOTING_PERIOD_LENGTH)
+    assert.equal(await moloch.gracePeriodLength(), configJSON.GRACE_PERIOD_LENGTH)
+    assert.equal(await moloch.proposalDeposit(), configJSON.PROPOSAL_DEPOSIT)
+    assert.equal(await moloch.dilutionBound(), configJSON.DILUTION_BOUND)
+    assert.equal(await moloch.processingReward(), configJSON.PROCESSING_REWARD)
+    assert.equal(+summoningTime, now)
+  })
+
+  it('submit proposal', async () => {
+    const now = await blockTime()    
+    const tx = await moloch.submitProposal(applicant, new BigNumber("10000000000000000000"), 3, "first proposal")
+    const event = await getEventParams(tx, "SubmitProposal")
+    assert.equal(+event[0], 0)
+    assert.equal(event[1], applicant)
+    assert.equal(event[2], summoner)
+  })
+
+})
