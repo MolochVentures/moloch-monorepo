b20b864a814c9c9c6b8e1b11abaf9ad3a84c5076
Rahul <rksethuram9@gmail.com>
2019-02-12 10:26:02 -0800

    Import web3 as docs specify

--- packages/moloch-backend/src/web3.ts
+++ packages/moloch-backend/src/web3.ts
@@ -1,9 +1,6 @@
-import * as W3 from 'web3';
-import Web3 from 'web3/types'
+import Web3 from 'web3';
 const molochArtifact = require('../../contracts/Moloch.json')
 
-// TODO: fix this, issue created: https://github.com/ethereum/web3.js/issues/2363
-// @ts-ignore
-const web3 = new W3(new W3.providers.HttpProvider('http://localhost:8545')) as Web3;
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:8545'));
 
 export const molochContract = new web3.eth.Contract(molochArtifact.abi, process.env.MOLOCH_ADDRESS)
