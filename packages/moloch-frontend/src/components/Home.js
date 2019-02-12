5e12d7e60df2fd9a9ec72f191d26105b7a4a3e33
Nuria <nuria.extremadouro@starshotsoftware.com>
2019-02-12 10:01:34 +0100

    Temporary branch to work on our end

--- packages/moloch-frontend/src/components/Home.js
+++ packages/moloch-frontend/src/components/Home.js
@@ -1,3 +1,171 @@
+// import React from 'react';
+// import { Grid, Button, Segment } from "semantic-ui-react";
+// import { Link } from 'react-router-dom';
+// import Graph from './Graph';
+// import moment from 'moment';
+
+// import { connect } from 'react-redux';
+// import { fetchMemberDetail, fetchMembersWithShares, fetchProposals, getAssetAmount } from '../action/actions';
+// import gql from 'graphql-tag';
+// import { Query } from 'react-apollo';
+
+// const formatter = new Intl.NumberFormat('en-US', {
+//   style: 'currency',
+//   currency: 'USD',
+//   minimumFractionDigits: 2
+// });
+
+// const GET_MEMBERS = gql`
+//   {
+//     members(where: { shares_gt: 0, isActive: true }) {
+//       id
+//     }
+//   }
+// `
+// const NumMembers = () => (
+//   <Query query={GET_MEMBERS}>
+//     {({ loading, error, data }) => {
+//       let members
+//       if (error) {
+//         members = 'NA'
+//         console.error(`Could not load members: ${error}`)
+//       } else if (loading) {
+//         members = '-'
+//       } else {
+//         members = data.members.length
+//       }
+//       return (
+//         <Link to='/members' className="link">
+//           <Button size='large' color='grey' className='btn_link'>{members} Members</Button>
+//         </Link>
+//       );
+//     }}
+//   </Query>
+// );
+
+// // TODO filter this to get current proposals?
+// const GET_PROPOSALS = gql`
+//   {
+//     proposals {
+//       id
+//     }
+//   }
+// `
+// const NumProposals = () => (
+//   <Query query={GET_PROPOSALS}>
+//     {({ loading, error, data }) => {
+//       let proposals
+//       if (error) {
+//         proposals = 'NA'
+//         console.error(`Could not load proposals: ${error}`)
+//       } else if (loading) {
+//         proposals = '-'
+//       } else {
+//         proposals = data.proposals.length
+//       }
+//       return (
+//         <Link to='/proposals' className="link">
+//           <Button size='large' color='grey' className='btn_link'>{proposals} Proposals</Button>
+//         </Link>
+//       );
+//     }}
+//   </Query>
+// );
+
+// class HomePage extends React.Component {
+//   state = {
+//     ethAmount: 0
+//   }
+
+//   componentDidMount() {
+//     if (localStorage.getItem('loggedUser')) {
+//       let loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+//       this.props.fetchMemberDetail(loggedUser.address)
+//         .then((responseJson) => {
+//           if (responseJson.type === 'FETCH_MEMBER_DETAIL_SUCCESS') {
+//             loggedUser.shares = responseJson.items.member.shares;
+//             loggedUser.status = responseJson.items.member.status;
+//             localStorage.setItem('loggedUser', JSON.stringify(loggedUser));
+//           }
+//         })
+//     }
+
+//     this.props.getAssetAmount()
+//       .then((responseJson) => {
+//         this.setState({ ethAmount: (responseJson.items) ? (responseJson.items.amount ? responseJson.items.amount : 0) : 0 })
+//       })
+//   }
+
+//   render() {
+//     return (
+//       <div id="homepage">
+//         <Grid columns={16} verticalAlign="middle">
+//           <Grid.Column mobile={16} tablet={6} computer={4} className="guild_value" >
+//             <Link to='/guildbank' className="text_link">
+//               <p className="subtext">Guild Bank Value</p>
+//               <p className="amount">{formatter.format(this.state.ethAmount)}</p>
+//             </Link>
+//           </Grid.Column>
+//           <Grid.Column mobile={16} tablet={10} computer={8} textAlign="center" className="browse_buttons" >
+//             <NumMembers />
+//             <NumProposals />
+//           </Grid.Column>
+
+//           <Grid.Column computer={4} />
+
+//           <Grid.Column width={16}>
+//             <Segment className="blurred box">
+//               <Grid columns="equal" className="graph_values">
+//                 <Grid.Column textAlign="left">
+//                   <p className="subtext">Total Shares</p>
+//                   <p className="amount">378</p>
+//                 </Grid.Column>
+//                 <Grid.Column textAlign="center">
+//                   <p className="subtext">Total ETH</p>
+//                   <p className="amount">541</p>
+//                 </Grid.Column>
+//                 <Grid.Column textAlign="right">
+//                   <p className="subtext">Share Value</p>
+//                   <p className="amount">128 USD</p>
+//                 </Grid.Column>
+//               </Grid>
+//               <div className="graph">
+//                 <Graph></Graph>
+//               </div>
+//             </Segment>
+//           </Grid.Column>
+//         </Grid>
+
+//       </div>
+//     )
+//   }
+// }
+
+// // This function is used to convert redux global state to desired props.
+// function mapStateToProps(state) {
+//   return {};
+// }
+
+// // This function is used to provide callbacks to container component.
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     fetchMemberDetail: function (id) {
+//       return dispatch(fetchMemberDetail(id));
+//     },
+//     fetchMembersWithShares: function () {
+//       return dispatch(fetchMembersWithShares());
+//     },
+//     fetchProposals: function (params) {
+//       return dispatch(fetchProposals(params));
+//     },
+//     getAssetAmount: function () {
+//       return dispatch(getAssetAmount());
+//     },
+//   };
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
+
 import React from 'react';
 import { Grid, Button, Segment } from "semantic-ui-react";
 import { Link } from 'react-router-dom';
@@ -6,8 +174,6 @@ import moment from 'moment';
 
 import { connect } from 'react-redux';
 import { fetchMemberDetail, fetchMembersWithShares, fetchProposals, getAssetAmount } from '../action/actions';
-import gql from 'graphql-tag';
-import { Query } from 'react-apollo';
 
 const formatter = new Intl.NumberFormat('en-US', {
   style: 'currency',
@@ -15,66 +181,14 @@ const formatter = new Intl.NumberFormat('en-US', {
   minimumFractionDigits: 2
 });
 
-const GET_MEMBERS = gql`
-  {
-    members(where: { shares_gt: 0, isActive: true }) {
-      id
-    }
-  }
-`
-const NumMembers = () => (
-  <Query query={GET_MEMBERS}>
-    {({ loading, error, data }) => {
-      let members
-      if (error) {
-        members = 'NA'
-        console.error(`Could not load members: ${error}`)
-      } else if (loading) {
-        members = '-'
-      } else {
-        members = data.members.length
-      }
-      return (
-        <Link to='/members' className="link">
-          <Button size='large' color='grey' className='btn_link'>{members} Members</Button>
-        </Link>
-      );
-    }}
-  </Query>
-);
-
-// TODO filter this to get current proposals?
-const GET_PROPOSALS = gql`
-  {
-    proposals {
-      id
-    }
-  }
-`
-const NumProposals = () => (
-  <Query query={GET_PROPOSALS}>
-    {({ loading, error, data }) => {
-      let proposals
-      if (error) {
-        proposals = 'NA'
-        console.error(`Could not load proposals: ${error}`)
-      } else if (loading) {
-        proposals = '-'
-      } else {
-        proposals = data.proposals.length
-      }
-      return (
-        <Link to='/proposals' className="link">
-          <Button size='large' color='grey' className='btn_link'>{proposals} Proposals</Button>
-        </Link>
-      );
-    }}
-  </Query>
-);
-
 class HomePage extends React.Component {
-  state = {
-    ethAmount: 0
+  constructor(props) {
+    super(props);
+    this.state = {
+      totalMembers: 0,
+      totalProposals: 0,
+      ethAmount: 0
+    }
   }
 
   componentDidMount() {
@@ -90,10 +204,29 @@ class HomePage extends React.Component {
         })
     }
 
+    this.props.fetchMembersWithShares()
+      .then((responseJson) => {
+        this.setState({ totalMembers: parseInt(responseJson.items) })
+      });
+
     this.props.getAssetAmount()
       .then((responseJson) => {
         this.setState({ ethAmount: (responseJson.items) ? (responseJson.items.amount ? responseJson.items.amount : 0) : 0 })
       })
+
+    let proposalParams = {
+      currentDate: moment(new Date()).format('YYYY-MM-DD')
+    }
+    let self = this;
+    this.props.fetchProposals(proposalParams)
+      .then((responseJson) => {
+        if (responseJson.items && Object.keys(responseJson.items).length > 0) {
+          // eslint-disable-next-line array-callback-return
+          Object.keys(responseJson.items).map((key, idx) => {
+            self.setState({ totalProposals: self.state.totalProposals+=responseJson.items[key].length });
+          })
+        }
+      });
   }
 
   render() {
@@ -107,8 +240,12 @@ class HomePage extends React.Component {
             </Link>
           </Grid.Column>
           <Grid.Column mobile={16} tablet={10} computer={8} textAlign="center" className="browse_buttons" >
-            <NumMembers />
-            <NumProposals />
+            <Link to='/members' className="link">
+              <Button size='large' color='grey' className='btn_link'>{this.state.totalMembers} Members</Button>
+            </Link>
+            <Link to='/proposals' className="link">
+              <Button size='large' color='grey' className='btn_link'>{this.state.totalProposals} Proposals</Button>
+            </Link>
           </Grid.Column>
 
           <Grid.Column computer={4} />
--- packages/moloch-frontend/src/components/MemberList.js
+++ packages/moloch-frontend/src/components/MemberList.js
@@ -1,3 +1,199 @@
+// import React from 'react';
+// import { Grid, Image, Divider } from 'semantic-ui-react';
+// import { Switch, Route, Link } from 'react-router-dom';
+
+// import MemberDetail from './MemberDetail';
+// import bull from 'assets/bull.png';
+// import hood from 'assets/hood.png';
+
+// import { connect } from 'react-redux';
+// import { fetchMembers, fetchConfigFounders, fetchMemberDetail } from '../action/actions';
+// import { Query } from 'react-apollo';
+// import gql from 'graphql-tag';
+
+// const MemberAvatar = ({ address, shares }) => (
+//   <Grid.Column mobile={5} tablet={3} computer={3} textAlign="center" className="member_avatar" title={address}  >
+//     <Link to={`/members/${address}`} className="uncolored">
+//       <Image src={hood} centered size='tiny' />
+//       <p className="name">{!address ? '' : (address.length > 10 ? address.substring(0, 10) + '...' : address)}</p>
+//       <p className="subtext">{shares} shares</p>
+//     </Link>
+//   </Grid.Column>
+// );
+
+// const GET_LOGGED_IN_USER = gql`
+//   query User($address: String!) {
+//     member(id: $address) {
+//       id,
+//       shares,
+//       isActive
+//     }
+//   }
+// `
+// class LoggedInUser extends React.Component {
+//   render() {
+//     let loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+//     return (
+//       <Query query={GET_LOGGED_IN_USER} variables={{ address: loggedUser.address }}>
+//         {({ loading, error, data }) => {
+//           if (loading) return "...";
+//           if (error) throw new Error(`Error!: ${error}`);
+//           return data.member && data.member.isActive
+//           ? <Link to={`/members/${data.member.id}`} className="uncolored">
+//               <Image centered src={bull} size='tiny' />
+//               <p className="name">{!data.member.id ? '' : (data.member.id.length > 10 ? data.member.id.substring(0, 10) + '...' : data.member.id)}</p>
+//               <p className="subtext">{data.member.shares ? data.member.shares : 0} shares</p>
+//             </Link>
+//           : <div />
+//         }}
+//       </Query>
+//     )
+//   }
+// }
+
+// const GET_ELDERS = gql`
+//   {
+//     members(where: { shares_gte: 100, isActive: true }) {
+//       id
+//       shares
+//     }
+//   }
+// `
+// const Elders = () => (
+//   <Query query={GET_ELDERS}>
+//     {({ loading, error, data }) => {
+//       if (loading) return "...";
+//       if (error) throw new Error(`Error!: ${error}`);
+//       return (
+//         data.members.length > 0 ?
+//         data.members.map((elder, idx) => <MemberAvatar address={elder.id} shares={elder.shares} key={idx} />) : 
+//         <>No elders to show.</>
+//       )
+//     }}
+//   </Query>
+// )
+
+// const GET_NON_ELDERS = gql`
+//   {
+//     members(where: { shares_lt: 100, isActive: true }) {
+//       id
+//       shares
+//     }
+//   }
+// `
+// const Contributors = () => (
+//   <Query query={GET_NON_ELDERS}>
+//     {({ loading, error, data }) => {
+//       if (loading) return "...";
+//       if (error) throw new Error(`Error!: ${error}`);
+//       return (
+//         data.members.length > 0 ?
+//         data.members.map((contributor, idx) => <MemberAvatar address={contributor.id} shares={contributor.shares} key={idx} />) : 
+//         <>No contributors to show.</>
+//       )
+//     }}
+//   </Query>
+// )
+
+// const GET_MEMBERS = gql`
+//   {
+//     members(where: { shares_gt: 0, isActive: true }) {
+//       id
+//     }
+//   }
+// `
+// const MemberList = () => (
+//   <Query query={GET_MEMBERS}>
+//     {({ loading, error, data }) => {
+//       let members
+//       if (error) {
+//         members = 'NA'
+//         console.error(`Could not load members: ${error}`)
+//       } else if (loading) {
+//         members = '-'
+//       } else {
+//         members = data.members.length
+//       }
+//       return (
+//         <div id="member_list">
+//           <Grid columns={16} verticalAlign="middle">
+//             <Grid.Column mobile={16} tablet={6} computer={6} textAlign="left" className="member_list_header">
+//               <p className="subtext">{members} Members</p>
+//               <p className="title">Ranking</p>
+//             </Grid.Column>
+
+//             {/* <Grid.Column mobile={16} tablet={10} computer={10} textAlign="right" className="submit_button">
+//               <Link to='/membershipproposalsubmission' className="link">
+//                 <Button size='large' color='red' disabled={(props.user.status === 'founder') ? true : false}>Membership Proposal</Button>
+//               </Link>
+//             </Grid.Column> */}
+//           </Grid>
+
+//           <Grid>
+//             <Grid.Column textAlign="center">
+//               <LoggedInUser />
+//             </Grid.Column>
+//           </Grid>
+//           <Grid className="member_item">
+//             <Grid.Row>
+//               <p style={{ paddingLeft: '1rem' }}>Elders (100+ shares)</p>
+//             </Grid.Row>
+//             <Divider />
+//             <Grid.Row className="members_row" centered>
+//               <Elders />
+//             </Grid.Row>
+//           </Grid>
+//           <Grid className="member_item">
+//             <Grid.Row>
+//               <p style={{ paddingLeft: '1rem' }}>Contributors</p>
+//             </Grid.Row>
+//             <Divider />
+//             <Grid.Row className="members_row" centered>
+//               <Contributors />
+//             </Grid.Row>
+//           </Grid>
+//         </div>
+//       );
+//     }}
+//   </Query>
+// );
+
+// const MemberListView = () => (
+//   <Switch>
+//     <Route 
+//       exact 
+//       path="/members" 
+//       render={() => <MemberList />} 
+//     />
+//     <Route path="/members/:name" component={MemberDetail} />
+//   </Switch>
+// )
+
+// // This function is used to convert redux global state to desired props.
+// function mapStateToProps(state) {
+//   return {
+//     members: state.members.items,
+//     elders: state.founders.items
+//   };
+// }
+
+// // This function is used to provide callbacks to container component.
+// function mapDispatchToProps(dispatch) {
+//   return {
+//     fetchMembers: function () {
+//       return dispatch(fetchMembers());
+//     },
+//     fetchConfigFounders: function () {
+//       return dispatch(fetchConfigFounders());
+//     },
+//     fetchMemberDetail: function (id) {
+//       return dispatch(fetchMemberDetail(id))
+//     }
+//   };
+// }
+
+// export default connect(mapStateToProps, mapDispatchToProps)(MemberListView);
+
 import React from 'react';
 import { Grid, Image, Divider } from 'semantic-ui-react';
 import { Switch, Route, Link } from 'react-router-dom';
@@ -8,8 +204,6 @@ import hood from 'assets/hood.png';
 
 import { connect } from 'react-redux';
 import { fetchMembers, fetchConfigFounders, fetchMemberDetail } from '../action/actions';
-import { Query } from 'react-apollo';
-import gql from 'graphql-tag';
 
 const MemberAvatar = ({ address, shares }) => (
   <Grid.Column mobile={5} tablet={3} computer={3} textAlign="center" className="member_avatar" title={address}  >
@@ -21,153 +215,100 @@ const MemberAvatar = ({ address, shares }) => (
   </Grid.Column>
 );
 
-const GET_LOGGED_IN_USER = gql`
-  query User($address: String!) {
-    member(id: $address) {
-      id,
-      shares,
-      isActive
+const MemberList = (props) => {
+  return (
+    <div id="member_list">
+      <Grid columns={16} verticalAlign="middle">
+        <Grid.Column mobile={16} tablet={6} computer={6} textAlign="left" className="member_list_header">
+          <p className="subtext">{props.totalMembers} Members</p>
+          <p className="title">Ranking</p>
+        </Grid.Column>
+
+        {/* <Grid.Column mobile={16} tablet={10} computer={10} textAlign="right" className="submit_button">
+          <Link to='/membershipproposalsubmission' className="link">
+            <Button size='large' color='red' disabled={(props.user.status === 'founder') ? true : false}>Membership Proposal</Button>
+          </Link>
+        </Grid.Column> */}
+      </Grid>
+
+      {props.user.status === 'founder' || props.user.status === 'active' ?
+        <Grid>
+          <Grid.Column textAlign="center">
+            <Link to={`/members/${props.user.name}`} className="uncolored">
+              <Image centered src={bull} size='tiny' />
+              <p className="name">{!props.user.name ? '' : (props.user.name.length > 10 ? props.user.name.substring(0, 10) + '...' : props.user.name)}</p>
+              <p className="subtext">{props.user.shares ? props.user.shares : 0} shares</p>
+            </Link>
+          </Grid.Column>
+        </Grid> : null
+      }
+      <Grid className="member_item">
+        <Grid.Row>
+          <p style={{ paddingLeft: '1rem' }}>Elders (100+ shares)</p>
+        </Grid.Row>
+        <Divider />
+        <Grid.Row className="members_row" centered>
+          {props.elders.length > 0 ?
+            props.elders.map((elder, idx) => <MemberAvatar {...elder} key={idx} />) : <>No elders to show.</>}
+        </Grid.Row>
+      </Grid>
+      <Grid className="member_item">
+        <Grid.Row>
+          <p style={{ paddingLeft: '1rem' }}>Contributors</p>
+        </Grid.Row>
+        <Divider />
+        <Grid.Row className="members_row" centered>
+          {props.members.length > 0 ?
+            props.members.map((contributor, idx) => <MemberAvatar {...contributor} key={idx} />) : <>No contributors to show.</>}
+        </Grid.Row>
+      </Grid>
+    </div>
+  )
+};
+
+class MemberListView extends React.Component {
+  constructor(props) {
+    let loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
+    super(props);
+    this.state = {
+      totalMembers: 0,
+      user: {
+        name: loggedUser.address,
+        shares: 0,
+        status: ''
+      }
     }
   }
-`
-class LoggedInUser extends React.Component {
+  componentDidMount() {
+    this.props.fetchMembers()
+      .then((responseJson) => {
+        this.setState({ totalMembers: this.state.totalMembers + responseJson.items.length })
+      });
+    this.props.fetchConfigFounders()
+      .then((responseJson) => {
+        this.setState({ totalMembers: this.state.totalMembers + responseJson.items.length })
+      });
+
+    this.props.fetchMemberDetail(this.state.user.name)
+      .then((responseJson) => {
+        if (responseJson.type === 'FETCH_MEMBER_DETAIL_SUCCESS') {
+          let user = this.state.user;
+          user.shares = responseJson.items.member.shares;
+          user.status = responseJson.items.member.status;
+          this.setState({ user: user });
+        }
+      })
+  }
   render() {
-    let loggedUser = JSON.parse(localStorage.getItem('loggedUser'));
     return (
-      <Query query={GET_LOGGED_IN_USER} variables={{ address: loggedUser.address }}>
-        {({ loading, error, data }) => {
-          if (loading) return "...";
-          if (error) throw new Error(`Error!: ${error}`);
-          return data.member && data.member.isActive
-          ? <Link to={`/members/${data.member.id}`} className="uncolored">
-              <Image centered src={bull} size='tiny' />
-              <p className="name">{!data.member.id ? '' : (data.member.id.length > 10 ? data.member.id.substring(0, 10) + '...' : data.member.id)}</p>
-              <p className="subtext">{data.member.shares ? data.member.shares : 0} shares</p>
-            </Link>
-          : <div />
-        }}
-      </Query>
+      <Switch>
+
+        <Route exact path="/members" render={(props) => <MemberList members={this.props.members} elders={this.props.elders} totalMembers={this.state.totalMembers} user={this.state.user} />} />
+        <Route path="/members/:name" component={MemberDetail} />
+      </Switch>
     )
   }
 }
-
-const GET_ELDERS = gql`
-  {
-    members(where: { shares_gte: 100, isActive: true }) {
-      id
-      shares
-    }
-  }
-`
-const Elders = () => (
-  <Query query={GET_ELDERS}>
-    {({ loading, error, data }) => {
-      if (loading) return "...";
-      if (error) throw new Error(`Error!: ${error}`);
-      return (
-        data.members.length > 0 ?
-        data.members.map((elder, idx) => <MemberAvatar address={elder.id} shares={elder.shares} key={idx} />) : 
-        <>No elders to show.</>
-      )
-    }}
-  </Query>
-)
-
-const GET_NON_ELDERS = gql`
-  {
-    members(where: { shares_lt: 100, isActive: true }) {
-      id
-      shares
-    }
-  }
-`
-const Contributors = () => (
-  <Query query={GET_NON_ELDERS}>
-    {({ loading, error, data }) => {
-      if (loading) return "...";
-      if (error) throw new Error(`Error!: ${error}`);
-      return (
-        data.members.length > 0 ?
-        data.members.map((contributor, idx) => <MemberAvatar address={contributor.id} shares={contributor.shares} key={idx} />) : 
-        <>No contributors to show.</>
-      )
-    }}
-  </Query>
-)
-
-const GET_MEMBERS = gql`
-  {
-    members(where: { shares_gt: 0, isActive: true }) {
-      id
-    }
-  }
-`
-const MemberList = () => (
-  <Query query={GET_MEMBERS}>
-    {({ loading, error, data }) => {
-      let members
-      if (error) {
-        members = 'NA'
-        console.error(`Could not load members: ${error}`)
-      } else if (loading) {
-        members = '-'
-      } else {
-        members = data.members.length
-      }
-      return (
-        <div id="member_list">
-          <Grid columns={16} verticalAlign="middle">
-            <Grid.Column mobile={16} tablet={6} computer={6} textAlign="left" className="member_list_header">
-              <p className="subtext">{members} Members</p>
-              <p className="title">Ranking</p>
-            </Grid.Column>
-
-            {/* <Grid.Column mobile={16} tablet={10} computer={10} textAlign="right" className="submit_button">
-              <Link to='/membershipproposalsubmission' className="link">
-                <Button size='large' color='red' disabled={(props.user.status === 'founder') ? true : false}>Membership Proposal</Button>
-              </Link>
-            </Grid.Column> */}
-          </Grid>
-
-          <Grid>
-            <Grid.Column textAlign="center">
-              <LoggedInUser />
-            </Grid.Column>
-          </Grid>
-          <Grid className="member_item">
-            <Grid.Row>
-              <p style={{ paddingLeft: '1rem' }}>Elders (100+ shares)</p>
-            </Grid.Row>
-            <Divider />
-            <Grid.Row className="members_row" centered>
-              <Elders />
-            </Grid.Row>
-          </Grid>
-          <Grid className="member_item">
-            <Grid.Row>
-              <p style={{ paddingLeft: '1rem' }}>Contributors</p>
-            </Grid.Row>
-            <Divider />
-            <Grid.Row className="members_row" centered>
-              <Contributors />
-            </Grid.Row>
-          </Grid>
-        </div>
-      );
-    }}
-  </Query>
-);
-
-const MemberListView = () => (
-  <Switch>
-    <Route 
-      exact 
-      path="/members" 
-      render={() => <MemberList />} 
-    />
-    <Route path="/members/:name" component={MemberDetail} />
-  </Switch>
-)
 
 // This function is used to convert redux global state to desired props.
 function mapStateToProps(state) {
