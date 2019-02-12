70de6cabfcc656527109e2aba60489642088d5cc
Rahul <rksethuram9@gmail.com>
2019-02-11 17:31:58 -0800

    Add graphQL

--- packages/moloch-frontend/src/App.js
+++ packages/moloch-frontend/src/App.js
@@ -13,25 +13,34 @@ import GuildBank from './components/GuildBank';
 import Login from './components/Login';
 import NotFound from './components/NotFound';
 import { store } from './store';
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
+
+const client = new ApolloClient({
+  uri: "http://127.0.0.1:8000/subgraphs/name/moloch"
+});
+
 const Provider = require('react-redux').Provider;
 
 const App = () => (
   <Router >
     <Provider store={store}>
-      <Background />
-      <Header />
-      <Wrapper>
-        <Switch>
-          <Route exact path="/" render={props => localStorage.getItem("loggedUser") ? <Home {...props}/> : <Redirect to={{pathname: '/login'}} />} />
-          <Route path="/proposals" render={props => localStorage.getItem("loggedUser") ? <ProposalList {...props}/> : <Redirect to={{pathname: '/login'}} />} />
-          <Route path="/members" render={props => localStorage.getItem("loggedUser") ? <MemberList {...props}/> : <Redirect to={{pathname: '/login'}} />} />
-          <Route path="/projectproposalsubmission" render={props => localStorage.getItem("loggedUser") ? <ProjectProposalSubmission {...props}/> : <Redirect to={{pathname: '/login'}} />} />
-          <Route path="/membershipproposalsubmission" render={props => localStorage.getItem("loggedUser") ? <MembershipProposalSubmission {...props}/> : <Redirect to={{pathname: '/login'}} />} />
-          <Route path="/guildbank" render={props => localStorage.getItem("loggedUser") ? <GuildBank {...props}/> : <Redirect to={{pathname: '/login'}} />} />
-          <Route path="/login" component={Login} />
-          <Route component={NotFound} />
-        </Switch>
-      </Wrapper>
+      <ApolloProvider client={client}>
+        <Background />
+        <Header />
+        <Wrapper>
+          <Switch>
+            <Route exact path="/" render={props => localStorage.getItem("loggedUser") ? <Home {...props}/> : <Redirect to={{pathname: '/login'}} />} />
+            <Route path="/proposals" render={props => localStorage.getItem("loggedUser") ? <ProposalList {...props}/> : <Redirect to={{pathname: '/login'}} />} />
+            <Route path="/members" render={props => localStorage.getItem("loggedUser") ? <MemberList {...props}/> : <Redirect to={{pathname: '/login'}} />} />
+            <Route path="/projectproposalsubmission" render={props => localStorage.getItem("loggedUser") ? <ProjectProposalSubmission {...props}/> : <Redirect to={{pathname: '/login'}} />} />
+            <Route path="/membershipproposalsubmission" render={props => localStorage.getItem("loggedUser") ? <MembershipProposalSubmission {...props}/> : <Redirect to={{pathname: '/login'}} />} />
+            <Route path="/guildbank" render={props => localStorage.getItem("loggedUser") ? <GuildBank {...props}/> : <Redirect to={{pathname: '/login'}} />} />
+            <Route path="/login" component={Login} />
+            <Route component={NotFound} />
+          </Switch>
+        </Wrapper>
+      </ApolloProvider>
     </Provider>
   </Router>
 );
