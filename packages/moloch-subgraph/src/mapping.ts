ae6c8a6cc374b993d7aca6911c5a7b35cb8733b6
James Young <james.s.young@gmail.com>
2019-02-13 18:17:45 -0700

    update proposal process

--- packages/moloch-subgraph/src/mapping.ts
+++ packages/moloch-subgraph/src/mapping.ts
@@ -1,28 +1,14 @@
 import { BigInt } from '@graphprotocol/graph-ts'
 import { Moloch as Contract, SummonComplete, SubmitProposal, SubmitVote, ProcessProposal, Ragequit, Abort, UpdateDelegateKey } from './types/Moloch/Moloch'
-import { Proposal, Member, Vote, Applicant } from './types/schema'
-
-export function handleSummonComplete(event: SummonComplete): void {
-  let member = new Member(event.params.summoner.toHex())
-  member.delegateKey = event.params.summoner
-  member.shares = event.params.shares
-  member.isActive = true
-  member.tokenTribute = BigInt.fromI32(0)
-  member.didRagequit = false
-  member.votes = new Array<string>()
-  member.submissions = new Array<string>()
-  member.save()
-}
+import { Proposal, Member, Vote, Applicant, Txs } from './types/schema'
 
 export function handleSubmitProposal(event: SubmitProposal): void {
   let proposal = new Proposal(event.params.proposalIndex.toString())
   proposal.timestamp = event.block.timestamp.toString()
   proposal.proposalIndex = event.params.proposalIndex
   proposal.delegateKey = event.params.delegateKey
-  proposal.member = event.params.memberAddress.toHex()
   proposal.memberAddress = event.params.memberAddress
-  proposal.applicant = event.params.applicant.toHex()
-  proposal.applicantAddress= event.params.applicant
+  proposal.applicant = event.params.applicant
   proposal.tokenTribute = event.params.tokenTribute
   proposal.sharesRequested = event.params.sharesRequested
   proposal.yesVotes = BigInt.fromI32(0)
@@ -37,23 +23,23 @@ export function handleSubmitProposal(event: SubmitProposal): void {
   applicant.timestamp = event.block.timestamp.toString()
   applicant.proposalIndex = event.params.proposalIndex
   applicant.delegateKey = event.params.delegateKey
-  applicant.member = event.params.memberAddress.toHex()
   applicant.memberAddress = event.params.memberAddress
-  applicant.applicantAddress = event.params.applicant
+  applicant.applicant = event.params.applicant
   applicant.tokenTribute = event.params.tokenTribute
   applicant.sharesRequested = event.params.sharesRequested
   applicant.didPass = false
-  applicant.aborted = false
   applicant.votes = new Array<string>()
-  applicant.proposal = event.params.proposalIndex.toString()
-  
   applicant.save()
+}
 
-  let member = Member.load(event.params.memberAddress.toHex())
-  let submission = event.params.proposalIndex.toString()
-  let memberSubmissions = member.submissions
-  memberSubmissions.push(submission)
-  member.submissions = memberSubmissions
+export function handleSummonComplete(event: SummonComplete): void {
+  let member = new Member(event.params.summoner.toHex())
+  member.delegateKey = event.params.summoner
+  member.shares = event.params.shares
+  member.isActive = true
+  member.didRagequit = false
+  member.votes = new Array<string>()
+  member.proposals = new Array<string>()
   member.save()
 }
 
@@ -66,8 +52,6 @@ export function handleSubmitVote(event: SubmitVote): void {
   vote.delegateKey = event.params.delegateKey
   vote.memberAddress = event.params.memberAddress
   vote.uintVote = event.params.uintVote
-  vote.proposal = event.params.proposalIndex.toString()
-  vote.member = event.params.memberAddress.toHex()
   vote.save()
 
   let proposal = Proposal.load(event.params.proposalIndex.toString())
@@ -77,49 +61,53 @@ export function handleSubmitVote(event: SubmitVote): void {
   if (event.params.uintVote == 2) {
     proposal.noVotes = proposal.noVotes.plus(BigInt.fromI32(1))
   }
-
-  let proposalVotes = proposal.votes
-  proposalVotes.push(voteID)
-  proposal.votes = proposalVotes
+  proposal.votes.push(voteID)
   proposal.save()
 
-  let applicant = Applicant.load(proposal.applicant)
-  let applicantVotes = applicant.votes
-  applicantVotes.push(voteID)
-  applicant.votes = applicantVotes
+  let applicant = Applicant.load(proposal.applicant.toHex())
+  applicant.votes.push(voteID)
   applicant.save()
   
   let member = Member.load(event.params.memberAddress.toHex())
-  let memberVotes = member.votes
-  memberVotes.push(voteID)
-  member.votes = memberVotes
+  member.votes.push(voteID)
+  member.proposals.push(event.params.proposalIndex.toString())
   member.save()
 }
 
 export function handleProcessProposal(event: ProcessProposal): void {
   let proposal = Proposal.load(event.params.proposalIndex.toString())
-  proposal.applicant = event.params.applicant.toHex()
+  proposal.applicant = event.params.applicant
   proposal.memberAddress = event.params.memberAddress
   proposal.tokenTribute = event.params.tokenTribute
   proposal.sharesRequested = event.params.sharesRequested
+  proposal.processed = true
   proposal.didPass = event.params.didPass
   proposal.save()
 
   if (event.params.didPass) {
-    let applicant = Applicant.load(event.params.applicant.toHex())
+    let timestamp = event.block.timestamp.toString()
+    let txs = Txs.load(event.params.memberAddress.toHex().concat("-").concat(timestamp))
+    if (txs == null) {
+      let txs = new Txs(event.params.memberAddress.toHex().concat("-").concat(timestamp))
+      txs.memberAddress = event.params.memberAddress
+      txs.tokenTribute = event.params.tokenTribute
+      txs.save()
+    }
+
+    let applicant = Applicant.load(proposal.applicant.toHex())
     applicant.didPass = true
     applicant.save()
 
     let member = Member.load(event.params.applicant.toHex())
     if (member == null) {
       let newMember = new Member(event.params.applicant.toHex())
-      newMember.delegateKey = event.params.applicant
       newMember.shares = event.params.sharesRequested
       newMember.isActive = true
+      newMember.highestIndexYesVote = BigInt.fromI32(0)
       newMember.tokenTribute = event.params.tokenTribute
       newMember.didRagequit = false
       member.votes = new Array<string>()
-      member.submissions = new Array<string>()
+      member.proposals = new Array<string>()
       newMember.save()
     } else {
       member.shares = member.shares.plus(event.params.sharesRequested)
@@ -134,6 +122,15 @@ export function handleRagequit(event: Ragequit): void {
   let member = Member.load(event.params.memberAddress.toHex())
   member.didRagequit = true
   member.save()
+
+  let timestamp = event.block.timestamp.toString()
+  let txs = Txs.load(event.params.memberAddress.toHex().concat("-").concat(timestamp))
+  if (txs == null) {
+    let txs = new Txs(event.params.memberAddress.toHex().concat("-").concat(timestamp))
+    txs.memberAddress = event.params.memberAddress
+    txs.tokenTribute = BigInt.fromI32(0).minus(event.params.sharesToBurn)
+    txs.save()
+  }
 }
 
 export function handleAbort(event: Abort): void {
