277fb4c2e6cea4f5915d9f191ec9158dc19b38c4
Rahul <rksethuram9@gmail.com>
2019-02-25 17:17:51 -0800

    Add mutation

--- packages/moloch-frontend/src/resolvers.js
+++ packages/moloch-frontend/src/resolvers.js
@@ -1,9 +1,26 @@
+import { ProposalStatus } from "./helpers/proposals";
+import gql from "graphql-tag";
+
 export const defaults = {
   loggedInUser: ""
 };
 
 export const resolvers = {
   Proposal: {
-    status: () => false
+    status: () => ProposalStatus.Unknown
+  },
+  Mutation: {
+    setStatus: (_, variables, { cache, getCacheKey }) => {
+      const id = getCacheKey({ __typename: 'Proposal', id: variables.id })
+      const fragment = gql`
+        fragment getStatus on Proposal {
+          status
+        }
+      `;
+      const proposal = cache.readFragment({ fragment, id });
+      const data = { ...proposal, status: variables.status };
+      cache.writeData({ id, data });
+      return data;
+    },
   }
 }
