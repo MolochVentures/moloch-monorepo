be31b4566dedc3408d2dcd4f38bbe878b04ce87e
Rahul <rksethuram9@gmail.com>
2019-03-03 16:53:06 -0800

    Rearchitect

--- packages/moloch-frontend/src/components/Home.js
+++ packages/moloch-frontend/src/components/Home.js
@@ -3,9 +3,9 @@ import { Grid, Button, Segment, Modal, Form } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import gql from "graphql-tag";
 import { Query, withApollo } from "react-apollo";
-import { getToken, getMedianizer, getWeb3, getMoloch } from "../web3";
-import { GET_EXCHANGE_RATE, GET_TOTAL_SHARES, GET_GUILD_BANK_VALUE } from "../helpers/graphQlQueries";
+import { getToken } from "../web3";
 import { utils } from "ethers";
+import { GET_METADATA } from "../helpers/graphQlQueries";
 
 const GET_MEMBERS = gql`
   {
@@ -72,65 +72,14 @@ class HomePage extends React.Component {
   state = {
     approval: "",
     token: null,
-    userAddress: null,
-    exchangeRate: 0,
-    totalShares: "0",
-    guildBankValue: "0"
+    userAddress: null
   };
 
   async componentDidMount() {
-    const { client } = this.props;
     const token = await getToken();
 
-    let { data: exchangeRateData } = await client.query({
-      query: GET_EXCHANGE_RATE
-    });
-    let rate = exchangeRateData.exchangeRate;
-    if (!rate) {
-      const medianizer = await getMedianizer();
-      rate = (await medianizer.compute())[0];
-      client.writeData({
-        data: {
-          exchangeRate: rate
-        }
-      });
-    }
-
-    let { data: sharesData } = await client.query({
-      query: GET_TOTAL_SHARES
-    });
-    let shares = sharesData.totalShares;
-    if (!shares) {
-      const moloch = await getMoloch();
-      shares = await moloch.totalShares();
-      console.log("shares: ", shares.toString());
-      client.writeData({
-        data: {
-          totalShares: shares.toString()
-        }
-      });
-    }
-
-    const eth = getWeb3();
-    let { data: guildBankData } = await client.query({
-      query: GET_GUILD_BANK_VALUE
-    });
-    let guildBankValue = guildBankData.guildBankValue;
-    if (!guildBankValue) {
-      guildBankValue = await eth.getBalance(process.env.REACT_APP_GUILD_BANK_ADDRESS);
-      client.writeData({
-        data: {
-          guildBankValue: guildBankValue.toString()
-        }
-      });
-    }
-
     this.setState({
-      eth,
-      token,
-      exchangeRate: rate,
-      totalShares: shares.toString(),
-      guildBankValue: guildBankValue.toString()
+      token
     });
   }
 
@@ -143,65 +92,80 @@ class HomePage extends React.Component {
   };
 
   render() {
-    const { approval, exchangeRate, totalShares, guildBankValue, eth } = this.state;
+    const { approval } = this.state;
     return (
-      <div id="homepage">
-        <Grid columns={16} verticalAlign="middle">
-          <Grid.Column mobile={16} tablet={6} computer={4} className="guild_value">
-            <Link to="/guildbank" className="text_link">
-              <p className="subtext">Guild Bank Value</p>
-              <p className="amount">${parseFloat(utils.formatEther(utils.bigNumberify(guildBankValue).mul(exchangeRate))).toFixed(2)}</p>
-            </Link>
-          </Grid.Column>
-          <Grid.Column mobile={16} tablet={10} computer={8} textAlign="center" className="browse_buttons">
-            <NumMembers />
-            <NumProposals />
-          </Grid.Column>
-          <Grid.Column mobile={16} tablet={6} computer={4} className="guild_value">
-            <Modal
-              basic
-              size="small"
-              trigger={
-                <Button size="large" color="grey" className="browse_buttons">
-                  Approve wETH
-                </Button>
-              }
-            >
-              <Modal.Header>Approve wETH</Modal.Header>
-              <Modal.Content>
-                <Form onSubmit={this.handleSubmit}>
-                  <Form.Field>
-                    <label>Amount to Approve</label>
-                    <input placeholder="Amount in Wei" name="amount" value={approval} onChange={this.handleChange} className="asset_amount" />
-                  </Form.Field>
-                  <Button type="submit" color="grey" className="btn_link">
-                    Submit
-                  </Button>
-                </Form>
-              </Modal.Content>
-            </Modal>
-          </Grid.Column>
+      <Query query={GET_METADATA}>
+        {({ loading, error, data }) => {
+          if (error) {
+            console.error(`Could not load page: ${error}`);
+          } else if (loading) {
+            return "Loading...";
+          } else {
+            const { guildBankValue, exchangeRate, totalShares, shareValue } = data
+            return (
+              <div id="homepage">
+                <Grid columns={16} verticalAlign="middle">
+                  <Grid.Column mobile={16} tablet={6} computer={4} className="guild_value">
+                    <Link to="/guildbank" className="text_link">
+                      <p className="subtext">Guild Bank Value</p>
+                      <p className="amount">${parseFloat(utils.formatEther(utils.bigNumberify(guildBankValue).mul(exchangeRate))).toFixed(2)}</p>
+                    </Link>
+                  </Grid.Column>
+                  <Grid.Column mobile={16} tablet={10} computer={8} textAlign="center" className="browse_buttons">
+                    <NumMembers />
+                    <NumProposals />
+                  </Grid.Column>
+                  <Grid.Column mobile={16} tablet={6} computer={4} className="guild_value">
+                    <Modal
+                      basic
+                      size="small"
+                      trigger={
+                        <Button size="large" color="grey" className="browse_buttons">
+                          Approve wETH
+                        </Button>
+                      }
+                    >
+                      <Modal.Header>Approve wETH</Modal.Header>
+                      <Modal.Content>
+                        <Form onSubmit={this.handleSubmit}>
+                          <Form.Field>
+                            <label>Amount to Approve</label>
+                            <input placeholder="Amount in Wei" name="amount" value={approval} onChange={this.handleChange} className="asset_amount" />
+                          </Form.Field>
+                          <Button type="submit" color="grey" className="btn_link">
+                            Submit
+                          </Button>
+                        </Form>
+                      </Modal.Content>
+                    </Modal>
+                  </Grid.Column>
 
-          <Grid.Column width={16}>
-            <Segment className="blurred box">
-              <Grid columns="equal" className="graph_values">
-                <Grid.Column textAlign="left">
-                  <p className="subtext">Total Shares</p>
-                  <p className="amount">{totalShares}</p>
-                </Grid.Column>
-                <Grid.Column textAlign="center">
-                  <p className="subtext">Total ETH</p>
-                  <p className="amount">{utils.formatEther(guildBankValue)}</p>
-                </Grid.Column>
-                <Grid.Column textAlign="right">
-                  <p className="subtext">Share Value</p>
-                  <p className="amount">{utils.bigNumberify(guildBankValue).gt(0) ? utils.bigNumberify(totalShares).div(guildBankValue) : 0}</p>
-                </Grid.Column>
-              </Grid>
-            </Segment>
-          </Grid.Column>
-        </Grid>
-      </div>
+                  <Grid.Column width={16}>
+                    <Segment className="blurred box">
+                      <Grid columns="equal" className="graph_values">
+                        <Grid.Column textAlign="left">
+                          <p className="subtext">Total Shares</p>
+                          <p className="amount">{totalShares}</p>
+                        </Grid.Column>
+                        <Grid.Column textAlign="center">
+                          <p className="subtext">Total ETH</p>
+                          <p className="amount">{utils.formatEther(guildBankValue)}</p>
+                        </Grid.Column>
+                        <Grid.Column textAlign="right">
+                          <p className="subtext">Share Value</p>
+                          <p className="amount">
+                            {shareValue}
+                          </p>
+                        </Grid.Column>
+                      </Grid>
+                    </Segment>
+                  </Grid.Column>
+                </Grid>
+              </div>
+            );
+          }
+        }}
+      </Query>
     );
   }
 }
