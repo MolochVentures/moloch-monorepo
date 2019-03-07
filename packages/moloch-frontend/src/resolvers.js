7ae60fd8bbc91fc498950f62ce12503abae0f084
Rahul <rksethuram9@gmail.com>
2019-03-06 22:46:33 -0800

    Add vars to cache

--- packages/moloch-frontend/src/resolvers.js
+++ packages/moloch-frontend/src/resolvers.js
@@ -14,18 +14,32 @@ export const resolvers = {
   Proposal: {
     status: () => ProposalStatus.Unknown,
     title: () => "",
-    description: () => ""
+    description: () => "",
+    gracePeriod: () => "",
+    votingEnds: () => "",
+    votingStarts: () => "",
+    readyForProcessing: () => false
   },
   Mutation: {
     setAttributes: (_, variables, { cache, getCacheKey }) => {
       const id = getCacheKey({ __typename: "Proposal", id: variables.id });
+      console.log('variables: ', variables);
       const fragment = gql`
         fragment getStatus on Proposal {
           status
         }
       `;
       const proposal = cache.readFragment({ fragment, id });
-      const data = { ...proposal, status: variables.status, title: variables.title, description: variables.description };
+      const data = {
+        ...proposal,
+        status: variables.status,
+        title: variables.title,
+        description: variables.description,
+        gracePeriod: variables.gracePeriod,
+        votingEnds: variables.votingEnds,
+        votingStarts: variables.votingStarts,
+        readyForProcessing: variables.readyForProcessing
+      };
       cache.writeData({ id, data });
       return data;
     }
