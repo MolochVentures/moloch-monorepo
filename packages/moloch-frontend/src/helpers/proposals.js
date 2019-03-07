8961dea2ce4f8a66e5afafe48cbe2588be6b51a3
Rahul <rksethuram9@gmail.com>
2019-03-06 22:21:18 -0800

    Fix statuses

--- packages/moloch-frontend/src/helpers/proposals.js
+++ packages/moloch-frontend/src/helpers/proposals.js
@@ -1,7 +1,7 @@
-import { initMoloch, getMoloch } from "../web3";
+import { getMoloch } from "../web3";
 
-const VOTING_PERIOD_LENGTH = 7;
-const GRACE_PERIOD_LENGTH = 7;
+const VOTING_PERIOD_LENGTH = 35;
+const GRACE_PERIOD_LENGTH = 35;
 
 export const ProposalStatus = {
   Unknown: "Unknown",
@@ -10,13 +10,18 @@ export const ProposalStatus = {
   GracePeriod: "GracePeriod",
   Aborted: "Aborted",
   Passed: "Passed",
-  Failed: "Failed"
+  Failed: "Failed",
+  ReadyForProcessing: "ReadyForProcessing"
 };
 
 // fill in missing data from onchain
 export async function getProposalDetailsFromOnChain(proposal, currentPeriod) {
   const moloch = await getMoloch()
+  currentPeriod = parseInt(currentPeriod)
+  console.log('currentPeriod: ', currentPeriod);
   
+  const inQueue = proposal => currentPeriod < proposal.startingPeriod;
+
   const inGracePeriod = proposal =>
     currentPeriod > proposal.startingPeriod + VOTING_PERIOD_LENGTH &&
     currentPeriod < proposal.startingPeriod + VOTING_PERIOD_LENGTH + GRACE_PERIOD_LENGTH;
@@ -33,13 +38,13 @@ export async function getProposalDetailsFromOnChain(proposal, currentPeriod) {
     if (proposal.proposalIndex === 0) {
       return true
     }
-    const lastProposalFromChain = await moloch.methods.proposalQueue(proposal.proposalIndex - 1).call();
+    const lastProposalFromChain = await moloch.proposalQueue(proposal.proposalIndex - 1);
     return lastProposalFromChain.processed
   }
 
   proposal.proposalIndex = parseInt(proposal.proposalIndex);
 
-  const proposalFromChain = await moloch.methods.proposalQueue(proposal.proposalIndex).call();
+  const proposalFromChain = await moloch.proposalQueue(proposal.proposalIndex);
   console.log('proposalFromChain: ', proposalFromChain);
   proposal.startingPeriod = parseInt(proposalFromChain.startingPeriod);
 
@@ -53,16 +58,19 @@ export async function getProposalDetailsFromOnChain(proposal, currentPeriod) {
     proposal.status = ProposalStatus.Passed;
   } else if (proposal.processed && !proposal.didPass) {
     proposal.status = ProposalStatus.Failed;
-  } else if (!proposal.lastProposalProcessed) {
-    proposal.status = ProposalStatus.InQueue;
   } else if (inGracePeriod(proposal)) {
     proposal.status = ProposalStatus.GracePeriod;
     proposal.gracePeriod = proposal.startingPeriod + VOTING_PERIOD_LENGTH + GRACE_PERIOD_LENGTH - currentPeriod;
   } else if (inVotingPeriod(proposal)) {
     proposal.status = ProposalStatus.VotingPeriod;
     proposal.votingEnds = proposal.startingPeriod + VOTING_PERIOD_LENGTH - currentPeriod;
-  } else {
+  } else if (inQueue(proposal)) {
     proposal.status = ProposalStatus.InQueue;
+    proposal.votingStarts = proposal.startingPeriod - currentPeriod
+  } else if (passedVotingAndGrace(proposal)) {
+    proposal.status = ProposalStatus.ReadyForProcessing
+  } else {
+    proposal.status = ProposalStatus.Unknown
   }
 
   proposal.votingEnded = passedVoting(proposal);
@@ -92,6 +100,7 @@ export async function getProposalDetailsFromOnChain(proposal, currentPeriod) {
         2
       )}`
     );
+    details.title = proposalFromChain.details
   }
 
   proposal.title = details.title;
