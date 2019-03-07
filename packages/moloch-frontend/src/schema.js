1933f0913be76cead14f782f08cd2719071687bc
Rahul <rksethuram9@gmail.com>
2019-03-06 22:46:43 -0800

    Add extra client vars

--- packages/moloch-frontend/src/schema.js
+++ packages/moloch-frontend/src/schema.js
@@ -4,7 +4,11 @@ export const typeDefs = gql`
   extend type Proposal {
     status: String,
     title: String,
-    description: String
+    description: String,
+    gracePeriod: String,
+    votingEnds: String,
+    votingStarts: String,
+    readyForProcessing: Bool
   }
 
   extend type Query {
