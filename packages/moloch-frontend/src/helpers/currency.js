5f9497a9cb71471c067a660fcc967c7fc80d8a4f
Rahul <rksethuram9@gmail.com>
2019-03-07 19:32:41 -0800

    Fixes

--- packages/moloch-frontend/src/helpers/currency.js
+++ packages/moloch-frontend/src/helpers/currency.js
@@ -1,18 +1,21 @@
 import { utils } from "ethers";
 
-export const formatter = new Intl.NumberFormat("en-US", {
+export const currencyFormatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
   minimumFractionDigits: 2
 });
 
+/**
+ * Converts Wei amount to Dollars
+ * @param {*} weiAmount 
+ * @param {*} exchangeRate in Wei units
+ */
 export function convertWeiToDollars(weiAmount, exchangeRate) {
-  console.log('weiAmount: ', weiAmount);
   console.log('exchangeRate: ', exchangeRate);
-  console.log('utils.formatEther(weiAmount): ', utils.formatEther(weiAmount));
-  console.log('utils.formatEther(exchangeRate): ', utils.formatEther(exchangeRate));
+  console.log('weiAmount: ', weiAmount);
   const weitoEth = utils.formatEther(weiAmount)
   const exchWeiToEth = utils.formatEther(exchangeRate)
   
-  return formatter.format(parseFloat(weitoEth * exchWeiToEth))
+  return currencyFormatter.format(parseFloat(weitoEth * exchWeiToEth))
 }
