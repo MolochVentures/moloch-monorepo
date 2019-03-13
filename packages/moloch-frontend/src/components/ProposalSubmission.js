7e774113fbdd1f341faba34347c29b814d7b9176
Rahul <rksethuram9@gmail.com>
2019-03-12 17:40:53 -0700

    Fix submit proposal

--- packages/moloch-frontend/src/components/ProposalSubmission.js
+++ packages/moloch-frontend/src/components/ProposalSubmission.js
@@ -2,57 +2,6 @@ import React, { Component } from "react";
 import { Button, Divider, Form, Grid, Input, Segment, GridColumn } from "semantic-ui-react";
 import { getMoloch } from "../web3";
 
-const formatter = new Intl.NumberFormat("en-US", {
-  style: "currency",
-  currency: "USD",
-  minimumFractionDigits: 2
-});
-
-class AssetsFields extends Component {
-  constructor(props) {
-    super(props);
-
-    this.handleAsset = this.handleAsset.bind(this);
-    this.deleteAsset = this.deleteAsset.bind(this);
-  }
-
-  handleAsset(event, { value, name }) {
-    this.props.onHandleAsset({ value, name, assetIndex: this.props.assetIndex });
-  }
-
-  deleteAsset() {
-    this.props.onHandleDeleteAsset({ assetIndex: this.props.assetIndex });
-  }
-
-  render() {
-    return (
-      <Grid.Row className="asset_field_row">
-        {/* <Grid.Column mobile={14} tablet={5} computer={7} className="asset_field_grid membership"> */}
-        <Grid.Column width={2} textAlign="left" className="asset_field_grid membership">
-          {/* <Input name="asset" className="asset icon_asset" icon="ethereum" iconPosition="left" placeholder="ETH, BTC, Token address" onChange={this.handleAsset} type="text" /> */}
-          {/* <Input icon="ethereum" iconPosition="left" name="asset" className="asset icon_asset" type="text" value={this.props.assets.symbol} disabled={true} /> */}
-          <div className="subtext" style={{ paddingTop: 10, paddingRight: 10 }}>
-            {this.props.assets.symbol}
-          </div>
-        </Grid.Column>
-        {/* <Grid.Column mobile={2} tablet={1} computer={2} className="asset_field_grid mobile_delete_icon" textAlign="right">
-                    <div className="subtext">
-                        <Icon name='times' className="delete_icon" link onClick={this.deleteAsset} />
-                    </div>
-                </Grid.Column> */}
-        <Grid.Column mobile={14} tablet={10} computer={7} className="asset_field_grid">
-          <Input name="amount" className="asset_amount" placeholder="Enter Amount" type="number" onChange={this.handleAsset} />
-        </Grid.Column>
-        {/* <Grid.Column mobile={2} tablet={1} computer={2} className="asset_field_grid computer_delete_icon" textAlign="center">
-                    <div className="subtext">
-                        <Icon name='times' className="delete_icon" link onClick={this.deleteAsset} />
-                    </div>
-                </Grid.Column> */}
-      </Grid.Row>
-    );
-  }
-}
-
 export default class ProposalSubmission extends Component {
   constructor(props) {
     super(props);
@@ -61,29 +10,15 @@ export default class ProposalSubmission extends Component {
       address: props.loggedInUser,
       title: "",
       description: "",
-      shares: 0,
-      tribute: 0, // TODO: this will be calculated with the blockchain
-      assets: [
-        {
-          asset: "ETH",
-          symbol: "ETH",
-          amount: 0,
-          logo: "https://s2.coinmarketcap.com/static/img/coins/64x64/1027.png"
-        }
-      ],
-      formErrors: { title: "", description: "", assets: "", shares: "" },
+      shares: "",
+      tribute: "", // TODO: this will be calculated with the blockchain
+      fieldValidationErrors: { title: "", description: "", assets: "", shares: "" },
       titleValid: false,
       descriptionValid: false,
-      assetsValid: false,
+      tributeValid: false,
       sharesValid: false,
       formValid: false
     };
-
-    this.addAsset = this.addAsset.bind(this);
-    this.handleInput = this.handleInput.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleAsset = this.handleAsset.bind(this);
-    this.handleDeleteAsset = this.handleDeleteAsset.bind(this);
   }
 
   async componentDidMount() {
@@ -93,107 +28,59 @@ export default class ProposalSubmission extends Component {
     });
   }
 
-  validateField(fieldName, value) {
-    let fieldValidationErrors = this.state.formErrors;
-    let titleValid = this.state.titleValid;
-    let descriptionValid = this.state.descriptionValid;
-    let assetsValid = this.state.assetsValid;
-    let sharesValid = this.state.sharesValid;
+  validateField = (fieldName, value) => {
+    let { fieldValidationErrors, titleValid, descriptionValid, tributeValid, sharesValid } = this.state
 
     switch (fieldName) {
       case "title":
         titleValid = value !== "";
-        fieldValidationErrors.title = titleValid ? "" : "Title field is invalid";
+        fieldValidationErrors.title = titleValid ? "" : "Title is invalid";
         break;
       case "description":
         descriptionValid = value !== "";
-        fieldValidationErrors.description = descriptionValid ? "" : "Description field is invalid";
-        break;
-      case "assets":
-        Object.keys(value).map(key => {
-          for (var i in value[key]) {
-            if (Object.keys(value[key]).length <= 1) {
-              assetsValid = false;
-              return false;
-            } else {
-              if (value[key][i] === null || value[key][i] === "") {
-                assetsValid = false;
-                return false;
-              }
-            }
-          }
-          assetsValid = true;
-          return true;
-        });
-        fieldValidationErrors.assets = assetsValid ? "" : "Asset is invalid";
+        fieldValidationErrors.description = descriptionValid ? "" : "Description is invalid";
         break;
       case "shares":
         sharesValid = value > 0;
-        fieldValidationErrors.shares = sharesValid ? "" : "Shares field is invalid";
+        fieldValidationErrors.shares = sharesValid ? "" : "Shares is invalid";
+        break;
+      case "tribute":
+        sharesValid = value > 0;
+        fieldValidationErrors.tribute = tributeValid ? "" : "Tribute is invalid";
         break;
       default:
         break;
     }
     this.setState(
       {
-        formErrors: fieldValidationErrors,
-        titleValid: titleValid,
-        descriptionValid: descriptionValid,
-        assetsValid: assetsValid,
-        sharesValid: sharesValid
+        fieldValidationErrors,
+        titleValid,
+        descriptionValid,
+        tributeValid,
+        sharesValid
       },
       this.validateForm
     );
   }
 
-  validateForm() {
-    this.setState({ formValid: this.state.titleValid && this.state.descriptionValid && this.state.assetsValid && this.state.sharesValid });
+  validateForm = () => {
+    this.setState({ formValid: this.state.titleValid && this.state.descriptionValid && this.state.sharesValid });
   }
 
-  addAsset() {
-    let assets = this.state.assets ? this.state.assets : [];
-    assets.push({});
-
-    this.setState({ assets });
-  }
-
-  handleInput(event) {
+  handleInput = (event) => {
     let name = event.target.name;
     let value = event.target.value;
-    if (name === "shares") {
-      this.setState({ [name]: parseInt(value) }, () => {
-        this.validateField(name, value);
-      });
-    } else {
-      this.setState({ [event.target.name]: event.target.value }, () => {
-        this.validateField(name, value);
-      });
-    }
-  }
-
-  handleAsset(event) {
-    let assets = this.state.assets;
-    assets[event.assetIndex][event.name] = event.value;
-    this.setState({ assets }, () => {
-      this.validateField("assets", assets);
-    });
-    this.setState({ tribute: 0 * event.value });
-  }
-
-  handleDeleteAsset(event) {
-    let assets = this.state.assets;
-    assets.splice(event.assetIndex, 1);
-    this.setState({ assets }, () => {
-      this.validateField("assets", assets);
+    this.setState({ [event.target.name]: event.target.value }, () => {
+      this.validateField(name, value);
     });
   }
 
-  async handleSubmit() {
+  handleSubmit = async () => {
     const { moloch, formValid, address, title, description, shares, tribute } = this.state
 
     if (formValid) {
       try {
-        await moloch.methods.submitProposal(address, tribute, shares, JSON.stringify({ title, description })).send({ from: address })
+        await moloch.submitProposal(address, tribute, shares, JSON.stringify({ title, description }))
       } catch (e) {
         console.error(e);
         alert("Error processing proposal");
@@ -204,6 +91,7 @@ export default class ProposalSubmission extends Component {
   }
 
   render() {
+    const { shares, tribute, title, description } = this.state
     return (
       <div id="proposal_submission">
         <Form>
@@ -217,7 +105,7 @@ export default class ProposalSubmission extends Component {
                   inverted
                   placeholder="Proposal Title"
                   onChange={this.handleInput}
-                  value={this.state.title}
+                  value={title}
                 />
                 <Divider />
               </Grid.Column>
@@ -227,12 +115,21 @@ export default class ProposalSubmission extends Component {
                 <Segment className="blurred box">
                   <Form.Input
                     name="shares"
-                    label="Request voting shares"
+                    label="Shares Requested"
                     placeholder="Shares"
                     fluid
                     type="number"
                     onChange={this.handleInput}
-                    value={this.state.shares}
+                    value={shares}
+                  />
+                  <Form.Input
+                    name="tribute"
+                    label="Tribute Offered (in ETH)"
+                    placeholder="ETH"
+                    fluid
+                    type="number"
+                    onChange={this.handleInput}
+                    value={tribute}
                   />
                 </Segment>
               </Grid.Column>
@@ -248,42 +145,8 @@ export default class ProposalSubmission extends Component {
                         placeholder="Type here"
                         rows={15}
                         onChange={this.handleInput}
-                        value={this.state.description}
+                        value={description}
                       />
-                    </Segment>
-                  </Grid.Column>
-                  <Grid.Column mobile={16} tablet={16} computer={8}>
-                    <Segment className="blurred box">
-                      <Grid columns={16}>
-                        <Grid.Column width={14}>
-                          <div className="subtext">Request Amount</div>
-                        </Grid.Column>
-                        {/* <Grid.Column width={2}>
-                                                    <div className="subtext">
-                                                        <Icon name='add' className="add_icon" link onClick={this.addAsset} />
-                                                    </div>
-                                                </Grid.Column> */}
-                      </Grid>
-                      <Grid columns={3} className="assets_field">
-                        {this.state.assets.map((row, i) => (
-                          <AssetsFields
-                            key={i}
-                            assetIndex={i}
-                            assets={row}
-                            onHandleAsset={this.handleAsset}
-                            onHandleDeleteAsset={this.handleDeleteAsset}
-                          />
-                        ))}
-                      </Grid>
-                      <Divider />
-                      <Grid columns="equal" className="value_shares">
-                        <Grid.Row>
-                          <Grid.Column textAlign="center">
-                            <p className="subtext">Tribute Value</p>
-                            <p className="amount">{formatter.format(this.state.tribute)}</p>
-                          </Grid.Column>
-                        </Grid.Row>
-                      </Grid>
                     </Segment>
                   </Grid.Column>
                 </Grid>
