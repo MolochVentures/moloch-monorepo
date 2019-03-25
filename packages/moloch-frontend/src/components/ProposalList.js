dcc9efc514d08fb645da19c6c37231dd24e7dddf
Rahul <rksethuram9@gmail.com>
2019-03-24 20:45:42 -0700

    Refactor list to use resolvers

--- packages/moloch-frontend/src/components/ProposalList.js
+++ packages/moloch-frontend/src/components/ProposalList.js
@@ -4,29 +4,30 @@ import { Route, Switch, Link } from "react-router-dom";
 
 import ProposalDetail, { Vote } from "./ProposalDetail";
 import ProgressBar from "./ProgressBar";
-import { Query, withApollo } from "react-apollo";
-import { getProposalDetailsFromOnChain, ProposalStatus, getProposalCountdownText } from "../helpers/proposals";
-import { SET_PROPOSAL_ATTRIBUTES, GET_PROPOSAL_LIST, GET_METADATA, GET_MEMBERS } from "../helpers/graphQlQueries";
+import { Query } from "react-apollo";
+import { ProposalStatus, getProposalCountdownText } from "../helpers/proposals";
+import { GET_PROPOSAL_LIST, GET_METADATA, GET_MEMBERS } from "../helpers/graphQlQueries";
 import { utils } from "ethers";
+import { adopt } from "react-adopt";
 
 const ProposalCard = ({ proposal }) => {
   let id = proposal.id;
 
   const yesShares = proposal.votes.reduce((totalVotes, vote) => {
     if (vote.uintVote === Vote.Yes) {
-      return totalVotes += parseInt(vote.member.shares)
+      return (totalVotes += parseInt(vote.member.shares));
     } else {
-      return totalVotes
+      return totalVotes;
     }
-  }, 0)
+  }, 0);
 
   const noShares = proposal.votes.reduce((totalVotes, vote) => {
     if (vote.uintVote === Vote.No) {
-      return totalVotes += parseInt(vote.member.shares)
+      return (totalVotes += parseInt(vote.member.shares));
     } else {
-      return totalVotes
+      return totalVotes;
     }
-  }, 0)
+  }, 0);
 
   return (
     <Grid.Column mobile={16} tablet={8} computer={5}>
@@ -41,7 +42,9 @@ const ProposalCard = ({ proposal }) => {
                 <p className="amount">{proposal.sharesRequested}</p>
               </Grid.Column>
               <Grid.Column textAlign="center">
-                <p className="subtext">Tribute <Icon name="ethereum" /></p>
+                <p className="subtext">
+                  Tribute <Icon name="ethereum" />
+                </p>
                 <p className="amount">{utils.formatEther(proposal.tokenTribute)}</p>
               </Grid.Column>
             </Grid.Row>
@@ -62,205 +65,128 @@ const ProposalCard = ({ proposal }) => {
   );
 };
 
+const Composed = adopt({
+  proposalsResult: ({ render }) => <Query query={GET_PROPOSAL_LIST}>{render}</Query>,
+  metadata: ({ render }) => <Query query={GET_METADATA}>{render}</Query>
+});
+
 class ProposalList extends React.Component {
   state = {
     proposals: [],
-    loading: true,
     totalShares: 0,
     shareValue: 0,
     exchangeRate: "0"
   };
 
-  async componentDidMount() {
-    this.fetchData();
-  }
-
-  async fetchData() {
-    const { client } = this.props;
-    this.setState({
-      loading: true
-    });
-    const { data: proposalData } = await client.query({
-      query: GET_PROPOSAL_LIST
-    });
-
-    const { data: metadata } = await client.query({
-      query: GET_METADATA
-    });
-
-    await this.determineProposalStatuses(client, proposalData.proposals, metadata.currentPeriod);
-    this.setState({
-      loading: false,
-      totalShares: +metadata.totalShares,
-      shareValue: metadata.shareValue,
-      exchangeRate: metadata.exchangeRate
-    });
-  }
-
-  determineProposalStatuses = async (client, proposals, currentPeriod) => {
-    if (!proposals || proposals.length === 0) {
-      return;
-    }
-
-    const fullProps = [];
-
-    // parallelize data fetch
-    await Promise.all(
-      proposals.map(async proposal => {
-        if (proposal.status === ProposalStatus.Unknown) {
-          try {
-            const fullProp = await getProposalDetailsFromOnChain(proposal, currentPeriod);
-            const result = await client.mutate({
-              mutation: SET_PROPOSAL_ATTRIBUTES,
-              variables: {
-                id: proposal.id,
-                status: fullProp.status,
-                title: fullProp.title,
-                description: fullProp.description,
-                gracePeriod: fullProp.gracePeriod,
-                votingEnds: `${fullProp.votingEnds}`,
-                votingStarts: `${fullProp.votingStarts}`,
-                readyForProcessing: fullProp.readyForProcessing
-              }
-            });
-            fullProps.push({
-              ...proposal,
-              status: result.data.setAttributes.status,
-              title: result.data.setAttributes.title,
-              description: result.data.setAttributes.description,
-              gracePeriod: result.data.setAttributes.gracePeriod,
-              votingEnds: result.data.setAttributes.votingEnds,
-              votingStarts: result.data.setAttributes.votingStarts,
-              readyForProcessing: result.data.setAttributes.readyForProcessing
-            });
-          } catch (e) {
-            console.error(e)
-          }
-        } else {
-          fullProps.push(proposal);
-        }
-      })
-    );
-
-    this.setState({
-      proposals: fullProps
-    });
-    return;
-  };
-
   render() {
     const { isActive } = this.props;
-    const { proposals, totalShares, loading, shareValue, exchangeRate } = this.state;
-
-    // sort in descending order of index
-    const sortProposals = (a, b) => b.proposalIndex - a.proposalIndex
-
-    const gracePeriod = proposals.filter(p => p.status === ProposalStatus.GracePeriod).sort(sortProposals);
-    const votingPeriod = proposals.filter(p => p.status === ProposalStatus.VotingPeriod).sort(sortProposals);
-    const inQueue = proposals.filter(p => p.status === ProposalStatus.InQueue).sort(sortProposals);
-    const readyForProcessing = proposals.filter(p => p.status === ProposalStatus.ReadyForProcessing).sort(sortProposals);
-    const completed = proposals.filter(
-      p => p.status === ProposalStatus.Aborted || p.status === ProposalStatus.Passed || p.status === ProposalStatus.Failed
-    ).sort(sortProposals);
-
-    const panes = [
-      {
-        menuItem: `Voting Period (${loading ? "..." : votingPeriod.length})`,
-        render: () => (
-          <Tab.Pane attached={false}>
-            {this.state.loading ? (
-              <>Loading proposals...</>
-            ) : (
-              <Grid columns={3}>
-                {votingPeriod.map((p, index) => (
-                  <ProposalCard exchangeRate={exchangeRate} shareValue={shareValue} totalShares={totalShares} proposal={p} key={index} />
-                ))}
-              </Grid>
-            )}
-          </Tab.Pane>
-        )
-      },
-      {
-        menuItem: `Grace Period (${loading ? "..." : gracePeriod.length})`,
-        render: () => (
-          <Tab.Pane attached={false}>
-            <Grid columns={3}>
-              {gracePeriod.map((p, index) => (
-                <ProposalCard exchangeRate={exchangeRate} shareValue={shareValue} totalShares={totalShares} proposal={p} key={index} />
-              ))}
-            </Grid>
-          </Tab.Pane>
-        )
-      },
-      {
-        menuItem: `Ready For Processing (${loading ? "..." : readyForProcessing.length})`,
-        render: () => (
-          <Tab.Pane attached={false}>
-            {this.state.loading ? (
-              <>Loading proposals...</>
-            ) : (
-              <Grid columns={3}>
-                {readyForProcessing.map((p, index) => (
-                  <ProposalCard exchangeRate={exchangeRate} shareValue={shareValue} totalShares={totalShares} proposal={p} key={index} />
-                ))}
-              </Grid>
-            )}
-          </Tab.Pane>
-        )
-      },
-      {
-        menuItem: `Completed (${loading ? "..." : completed.length})`,
-        render: () => (
-          <Tab.Pane attached={false}>
-            {this.state.loading ? (
-              <>Loading proposals...</>
-            ) : (
-              <Grid columns={3}>
-                {completed.map((p, index) => (
-                  <ProposalCard exchangeRate={exchangeRate} shareValue={shareValue} totalShares={totalShares} proposal={p} key={index} />
-                ))}
-              </Grid>
-            )}
-          </Tab.Pane>
-        )
-      },
-      {
-        menuItem: `In Queue (${loading ? "..." : inQueue.length})`,
-        render: () => (
-          <Tab.Pane attached={false}>
-            {this.state.loading ? (
-              <>Loading proposals...</>
-            ) : (
-              <Grid columns={3}>
-                {inQueue.map((p, index) => (
-                  <ProposalCard exchangeRate={exchangeRate} shareValue={shareValue} totalShares={totalShares} proposal={p} key={index} />
-                ))}
-              </Grid>
-            )}
-          </Tab.Pane>
-        )
-      }
-    ];
+    // const { proposals, totalShares, loading, shareValue, exchangeRate } = this.state;
 
     return (
-      <div id="proposal_list">
-        <>
-          <Grid columns={16} verticalAlign="middle">
-            <Grid.Column mobile={16} tablet={8} computer={4} textAlign="right" floated="right" className="submit_button">
-              <Link to={isActive ? "/proposalsubmission" : "/proposals"} className="link">
-                <Button size="large" color="red" disabled={!isActive}>
-                  New Proposal
-                </Button>
-              </Link>
-            </Grid.Column>
-          </Grid>
-          <Tab menu={{ secondary: true, pointing: true, vertical: true }} panes={panes} />
-        </>
-      </div>
+      <Composed id={this.props.match.params.id}>
+        {({ proposalsResult, metadata }) => {
+          if (proposalsResult.loading || metadata.loading) return <Segment className="blurred box">Loading...</Segment>;
+          if (proposalsResult.error) throw new Error(`Error!: ${proposalsResult.error}`);
+          if (metadata.error) throw new Error(`Error!: ${metadata.error}`);
+
+          const { proposals } = proposalsResult.data
+          const { shareValue, exchangeRate, totalShares } = metadata.data
+
+          // sort in descending order of index
+          const sortProposals = (a, b) => b.proposalIndex - a.proposalIndex;
+
+          const gracePeriod = proposals.filter(p => p.status === ProposalStatus.GracePeriod).sort(sortProposals);
+          const votingPeriod = proposals.filter(p => p.status === ProposalStatus.VotingPeriod).sort(sortProposals);
+          const inQueue = proposals.filter(p => p.status === ProposalStatus.InQueue).sort(sortProposals);
+          const readyForProcessing = proposals.filter(p => p.status === ProposalStatus.ReadyForProcessing).sort(sortProposals);
+          const completed = proposals
+            .filter(p => p.status === ProposalStatus.Aborted || p.status === ProposalStatus.Passed || p.status === ProposalStatus.Failed)
+            .sort(sortProposals);
+
+          const panes = [
+            {
+              menuItem: `Voting Period (${votingPeriod.length})`,
+              render: () => (
+                <Tab.Pane attached={false}>
+                  <Grid columns={3}>
+                    {votingPeriod.map((p, index) => (
+                      <ProposalCard exchangeRate={exchangeRate} shareValue={shareValue} totalShares={+totalShares} proposal={p} key={index} />
+                    ))}
+                  </Grid>
+                </Tab.Pane>
+              )
+            },
+            {
+              menuItem: `Grace Period (${gracePeriod.length})`,
+              render: () => (
+                <Tab.Pane attached={false}>
+                  <Grid columns={3}>
+                    {gracePeriod.map((p, index) => (
+                      <ProposalCard exchangeRate={exchangeRate} shareValue={shareValue} totalShares={+totalShares} proposal={p} key={index} />
+                    ))}
+                  </Grid>
+                </Tab.Pane>
+              )
+            },
+            {
+              menuItem: `Ready For Processing (${readyForProcessing.length})`,
+              render: () => (
+                <Tab.Pane attached={false}>
+                  <Grid columns={3}>
+                    {readyForProcessing.map((p, index) => (
+                      <ProposalCard exchangeRate={exchangeRate} shareValue={shareValue} totalShares={+totalShares} proposal={p} key={index} />
+                    ))}
+                  </Grid>
+                </Tab.Pane>
+              )
+            },
+            {
+              menuItem: `Completed (${completed.length})`,
+              render: () => (
+                <Tab.Pane attached={false}>
+                  <Grid columns={3}>
+                    {completed.map((p, index) => (
+                      <ProposalCard exchangeRate={exchangeRate} shareValue={shareValue} totalShares={+totalShares} proposal={p} key={index} />
+                    ))}
+                  </Grid>
+                </Tab.Pane>
+              )
+            },
+            {
+              menuItem: `In Queue (${inQueue.length})`,
+              render: () => (
+                <Tab.Pane attached={false}>
+                  <Grid columns={3}>
+                    {inQueue.map((p, index) => (
+                      <ProposalCard exchangeRate={exchangeRate} shareValue={shareValue} totalShares={+totalShares} proposal={p} key={index} />
+                    ))}
+                  </Grid>
+                </Tab.Pane>
+              )
+            }
+          ];
+
+          return (
+            <div id="proposal_list">
+              <>
+                <Grid columns={16} verticalAlign="middle">
+                  <Grid.Column mobile={16} tablet={8} computer={4} textAlign="right" floated="right" className="submit_button">
+                    <Link to={isActive ? "/proposalsubmission" : "/proposals"} className="link">
+                      <Button size="large" color="red" disabled={!isActive}>
+                        New Proposal
+                      </Button>
+                    </Link>
+                  </Grid.Column>
+                </Grid>
+                <Tab menu={{ secondary: true, pointing: true, vertical: true }} panes={panes} />
+              </>
+            </div>
+          );
+        }}
+      </Composed>
     );
   }
 }
-const ProposalListHOC = withApollo(ProposalList);
 
 const ProposalListView = ({ loggedInUser }) => {
   return (
@@ -268,10 +194,10 @@ const ProposalListView = ({ loggedInUser }) => {
       {({ loading, error, data }) => {
         if (loading) return "Loading...";
         if (error) throw new Error(`Error!: ${error}`);
-        const member = data.members.find(m => m.delegateKey === loggedInUser)
+        const member = data.members.find(m => m.delegateKey === loggedInUser);
         return (
           <Switch>
-            <Route exact path="/proposals" render={props => <ProposalListHOC {...props} isActive={member ? member.isActive : false} />} />
+            <Route exact path="/proposals" render={props => <ProposalList {...props} isActive={member ? member.isActive : false} />} />
             <Route path="/proposals/:id" render={props => <ProposalDetail {...props} loggedInUser={loggedInUser} />} />
           </Switch>
         );
