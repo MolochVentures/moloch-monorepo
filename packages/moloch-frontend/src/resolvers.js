34ded8262a52e2c3d33a010e4e0f75ff621d75b1
Rahul <rksethuram9@gmail.com>
2019-03-24 20:18:07 -0700

    Use local resolvers

--- packages/moloch-frontend/src/resolvers.js
+++ packages/moloch-frontend/src/resolvers.js
@@ -1,15 +1,69 @@
-import { ProposalStatus } from "./helpers/proposals";
+import {
+  ProposalStatus,
+  determineProposalStatus,
+  inGracePeriod,
+  VOTING_PERIOD_LENGTH,
+  GRACE_PERIOD_LENGTH,
+  inVotingPeriod,
+  inQueue,
+  passedVotingAndGrace
+} from "./helpers/proposals";
 import gql from "graphql-tag";
+import { GET_METADATA, GET_PROPOSAL_DETAIL } from "./helpers/graphQlQueries";
+import { computePublicKey } from "ethers/utils";
 
 export const resolvers = {
   Proposal: {
-    status: () => ProposalStatus.Unknown,
-    title: () => "",
-    description: () => "",
-    gracePeriod: () => 0,
-    votingEnds: () => 0,
-    votingStarts: () => 0,
-    readyForProcessing: () => false
+    status: (proposal, _args, { cache }) => {
+      const { currentPeriod } = cache.readQuery({ query: GET_METADATA });
+      return determineProposalStatus(proposal, +currentPeriod);
+    },
+    title: proposal => {
+      try {
+        const details = JSON.parse(proposal.details);
+        return details.title || proposal.details || "";
+      } catch (e) {
+        console.log(`Could not parse details from proposal.proposalIndex: ${proposal.proposalIndex} proposal.details: ${proposal.details}`);
+        return "";
+      }
+    },
+    description: proposal => {
+      try {
+        const details = JSON.parse(proposal.details);
+        return details.description || "";
+      } catch (e) {
+        console.log(`Could not parse details from proposal.proposalIndex: ${proposal.proposalIndex} proposal.details: ${proposal.details}`);
+        return "";
+      }
+    },
+    gracePeriod: (proposal, _args, { cache }) => {
+      const { currentPeriod } = cache.readQuery({ query: GET_METADATA });
+      if (inGracePeriod(proposal, currentPeriod)) {
+        return +proposal.startingPeriod + VOTING_PERIOD_LENGTH + GRACE_PERIOD_LENGTH - currentPeriod;
+      }
+      return 0;
+    },
+    votingEnds: (proposal, _args, { cache }) => {
+      const { currentPeriod } = cache.readQuery({ query: GET_METADATA });
+      if (inVotingPeriod(proposal, currentPeriod)) {
+        return proposal.startingPeriod + VOTING_PERIOD_LENGTH - currentPeriod;
+      }
+      return 0;
+    },
+    votingStarts: (proposal, _args, { cache }) => {
+      const { currentPeriod } = cache.readQuery({ query: GET_METADATA });
+      if (inQueue(proposal, currentPeriod)) {
+        return proposal.startingPeriod - currentPeriod;
+      }
+      return 0;
+    },
+    readyForProcessing: (proposal, _args, { cache }) => {
+      const { currentPeriod } = cache.readQuery({ query: GET_METADATA });
+      if (passedVotingAndGrace(proposal, currentPeriod) && !proposal.processed) {
+        return true;
+      }
+      return false
+    }
   },
   Mutation: {
     setAttributes: (_, variables, { cache }) => {
