16a95da31bd476a1fb7c9ce6619243e01e218017
James Young <james.s.young@gmail.com>
2019-05-28 08:46:28 -0600

    Updates

---
+++ precache-manifest.4ffa9edb248923d3fb87a76d74f7daaf.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = [
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/yangdao/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "3398e12db70c9ce5c7f3",
+    "url": "/yangdao/static/css/main.5c711023.chunk.css"
+  },
+  {
+    "revision": "27d59491936aad4ebd7e",
+    "url": "/yangdao/static/js/runtime~main.09d8fc30.js"
+  },
+  {
+    "revision": "d5fb9e9eb08306407b6d3fcaf0b4130a",
+    "url": "/yangdao/static/media/moloch_owl.d5fb9e9e.jpg"
+  },
+  {
+    "revision": "a3f367adb7065537096b",
+    "url": "/yangdao/static/js/2.1ce350e7.chunk.js"
+  },
+  {
+    "revision": "ba69d28c103dc761f05783293a4d18a3",
+    "url": "/yangdao/static/media/hood.ba69d28c.png"
+  },
+  {
+    "revision": "204229a6b7760c8830135263553acbe9",
+    "url": "/yangdao/static/media/bull.204229a6.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/yangdao/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/yangdao/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/yangdao/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/yangdao/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/yangdao/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "3398e12db70c9ce5c7f3",
+    "url": "/yangdao/static/js/main.4913e4f9.chunk.js"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/yangdao/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/yangdao/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/yangdao/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/yangdao/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/yangdao/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/yangdao/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/yangdao/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/yangdao/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/yangdao/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/yangdao/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "a3f367adb7065537096b",
+    "url": "/yangdao/static/css/2.2d669794.chunk.css"
+  },
+  {
+    "revision": "0c8f6a09812c2b8d0dc5466e5de62900",
+    "url": "/yangdao/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a39d5968d8717f98597166c0da420dae.js"
+  "/yangdao/precache-manifest.4ffa9edb248923d3fb87a76d74f7daaf.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/yangdao/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
