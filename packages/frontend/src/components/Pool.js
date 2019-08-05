27f09fec4a85df1906d3d9f9ee4c91e133799ec7
Rahul <rksethuram9@gmail.com>
2019-08-05 12:08:23 +0200

    Add donate UI

--- packages/frontend/src/components/Pool.js
+++ packages/frontend/src/components/Pool.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from "react";
-import { Grid, Button, Statistic, Loader, Popup } from "semantic-ui-react";
+import React, { useState, useEffect, useCallback } from "react";
+import { Grid, Button, Statistic, Loader, Popup, Modal, Header, Icon, Input } from "semantic-ui-react";
 import { Link } from "react-router-dom";
 import { Query } from "react-apollo";
 import { utils } from "ethers";
@@ -7,6 +7,9 @@ import { convertWeiToDollars } from "../helpers/currency";
 import gql from "graphql-tag";
 import { getMolochPool } from "web3";
 import { monitorTx } from "helpers/transaction";
+import { getToken } from "web3";
+import { formatEther } from "ethers/utils";
+import { EtherSymbol } from "ethers/constants";
 
 const NumMembers = () => (
   <Link to="/pool-members" className="link">
@@ -16,13 +19,54 @@ const NumMembers = () => (
   </Link>
 );
 
-const Donate = () => (
-  <Link to="/pool-donate" className="link">
-    <Button color="grey" size="medium" fluid>
-      Donate
-    </Button>
-  </Link>
-);
+const Donate = ({ token, molochPool, loggedInUser }) => {
+  const [donation, setDonation] = useState("");
+  const [myWeth, setMyWeth] = useState();
+
+  useEffect(() => {
+    async function fetchMyWeth() {
+      const weth = await token.balanceOf(loggedInUser);
+      setMyWeth(parseFloat(formatEther(weth)).toFixed(2));
+    }
+    fetchMyWeth();
+  }, [token, loggedInUser]);
+
+  const donate = useCallback(() => {
+    console.log("Calling molochPool.deposit with ", donation);
+    monitorTx(molochPool.deposit(donation));
+  }, [donation, molochPool]);
+
+  return (
+    <Modal
+      trigger={
+        <Button color="grey" size="medium" fluid>
+          Donate
+        </Button>
+      }
+      basic
+      size="small"
+      closeIcon
+    >
+      <Header content="Donate to the Moloch Pool" />
+      <Modal.Content>
+        <p>Thank you for your donation! Use the wETH Center to wrap and approve.</p>
+        <Input
+          inverted
+          labelPosition="right"
+          label={`${myWeth}w${EtherSymbol}`}
+          placeholder="wETH to Donate"
+          onChange={event => setDonation(event.target.value)}
+          value={donation}
+        />
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="green" inverted onClick={donate}>
+          <Icon name="checkmark" /> Donate
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
+};
 
 const Sync = ({ molochPool, lastProcessedProposalIndex, currentPoolIndex }) => {
   const synced = currentPoolIndex >= lastProcessedProposalIndex;
@@ -64,15 +108,17 @@ const GET_POOL_METADATA = gql`
 `;
 
 export default function Pool({ pageQueriesLoading, loggedInUser }) {
-  console.log("pageQueriesLoading: ", pageQueriesLoading);
   const [molochPool, setMolochPool] = useState({});
+  const [token, setToken] = useState({});
 
   useEffect(() => {
-    async function getMoloch() {
+    async function fetchData() {
       const pool = await getMolochPool(loggedInUser);
+      const token = await getToken(loggedInUser);
       setMolochPool(pool);
+      setToken(token);
     }
-    getMoloch();
+    fetchData();
   }, [loggedInUser]);
 
   return (
@@ -105,7 +151,7 @@ export default function Pool({ pageQueriesLoading, loggedInUser }) {
                       <NumMembers />
                     </Grid.Column>
                     <Grid.Column>
-                      <Donate />
+                      <Donate token={token} molochPool={molochPool} loggedInUser={loggedInUser} />
                     </Grid.Column>
                     <Grid.Column>
                       <Sync
